63fc19603cc21173a7c31f635c974b3f7bf91c1b
Diego <mottadiego93@gmail.com>
2022-07-07 15:12:31 -0300

    atualizações

---
+++ pokedex/src/pages/Pokedex.js
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import PokeCard from "../components/PokeCard";
+import GlobalStateContext from "../global/GlobalStateContext";
+
+const Pokedex = () => {
+  const states = useContext(GlobalStateContext);
+  const navigate = useNavigate();
+
+  const removePokemon = (pokemon) => {
+    const newListPokemon = states.pokedex.filter((poke) => {
+      return poke !== pokemon;
+    });
+    //envia de volta o poke para a home
+    states.setPokemonList([pokemon, ...states.pokemonList]);
+    //retira da pokedex
+    states.setPokedex(newListPokemon);
+  };
+
+  const functionDetails = (pokemon) => {
+    //envia para pág de detalhes
+    states.setPokemonDetails(pokemon);
+    //troca de pág
+    navigate("/details");
+  };
+
+  return (
+    <div>
+        {console.log(states.pokedex)}
+      {states.pokedex &&
+        states.pokedex.map((pokemon) => {
+          return (
+            <PokeCard
+              id={pokemon.id}
+              nome={pokemon.name}
+              img={pokemon.sprites.front_default}
+              remove={() => {
+                removePokemon(pokemon);
+              }}
+              details={() => {
+                functionDetails(pokemon);
+              }}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default Pokedex;
